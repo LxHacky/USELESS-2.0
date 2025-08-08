c560fcb61072623829e7fd010af0267e212c94d7
Harii <harikrishnamohan@proton.me>
2025-08-09 00:24:42 +0530

    function to draw line added

--- init.lua
+++ init.lua
@@ -1,11 +1,26 @@
 local print_ascii_art = require("ascii_chars")
-local colors = require("ansi_colors")
-local set_color = colors.set_color
 local json = require("dkjson")
 
-local function read_content(path)
-    local file = io.read()
-    local content = file:read()
+local colors = require("ansi_colors")
+local set_color = colors.set_color
+
+
+local function draw_line(char)
+    local width = tonumber(os.getenv("COLUMNS")) or 80 -- fallback to 80 if unknown
+    local line = string.rep(char, width)
+    print(line)
+end
+
+
+local function read_json(path)
+    if not path then return end
+    local file = io.open(path, "r")
+    if not file then
+        error("could not open " .. path)
+    end
+    local content = file:read("*a")
+    file:close()
+
     local table, _, err = json.decode(content, 1, nil)
     if err then
         error("Err parsing JSON: " .. err)
@@ -13,4 +28,9 @@ local function read_content(path)
     return table
 end
 
-local ppt_content = read_content("useless.json")
+local content = read_json("./useless.json")
+if not content then
+  error("could not open file ")
+end
+
+draw_line("#")
