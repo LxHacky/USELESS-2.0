ddebd888d315d481af826c08fc1597ae585b1e1f
Harii <harikrishnamohan@proton.me>
2025-08-08 23:42:34 +0530

    colors added

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
 local print_ascii_art = require("ascii_chars")
 local colors = require("ansi_colors")
 local set_color = colors.set_color
+local json = require("dkjson")
 
+local function read_content(path)
+    local file = io.read()
+    local content = file:read()
+    local table, _, err = json.decode(content, 1, nil)
+    if err then
+        error("Err parsing JSON: " .. err)
+    end
+    return table
+end
 
-local myname = io.read()
-print_ascii_art(myname)
+local ppt_content = read_content("useless.json")
